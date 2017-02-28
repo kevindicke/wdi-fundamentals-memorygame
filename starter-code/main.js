f16062c4d0dbd89277d2c213ca4ea518b95448a7
kevindicke <k.dicke7@gmail.com>
2017-02-28 07:52:38 -0800

    Card comparison added to js

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,11 @@ var cardONe = "king";
 var cardTwo = "king";
 var cardThree = "queen";
 var cardFour = "queen";
+
+if (cardONe === cardTwo) {
+	alert("You found a match!");
+}else if (cardThree === cardFour) {
+	alert("You found a match");
+}else{
+	alert("Sorry, try again.");
+}
